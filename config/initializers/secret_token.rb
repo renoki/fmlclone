783446348fc5cc99c668c1433411383b68687ba5
mark <renoki@marks-MacBook-Pro.local>
2011-02-27 20:19:31 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fmlclone::Application.config.secret_token = 'b0639ee15e5fda789c0d778418ae767cd36d869563736a3b032371a6bad0c08350fef1b14a935e8b656e7ae80fbb512ca4f23e57816aed99b14d913469193641'
