f54da2ca5651bcc3d7ea93716021df5bc66338cb
mark <renoki@marks-MacBook-Pro.local>
2011-03-06 16:55:27 -0800

    copied css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def title
+        base_title = "Broke as Hell"
+        if @title.nil?
+            base_title
+        else
+            "#{base_title} | #{@title}"
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Fmlclone::Application.routes.draw do
 
   get "pages/contact"
 
+  get "pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
