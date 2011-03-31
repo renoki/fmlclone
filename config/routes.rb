86c359f70a8f7002a3d0eb31f9dc37b20ccc7821
mark <renoki@marks-MacBook-Pro.local>
2011-03-30 22:58:36 -0700

    finished micropost

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Fmlclone::Application.routes.draw do
 
     resources :users
     resources :sessions, :only => [:new, :create, :destroy]
+    resources :microposts, :only => [:create, :destroy]
     match '/signin', :to => 'sessions#new'
     match '/signout', :to => 'sessions#destroy'
 
